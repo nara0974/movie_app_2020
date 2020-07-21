e841acb1caf5277d1f42cb6da357f672d6ebd28a
nara0974 <naas0974@gmail.com>
2020-07-21 16:23:05 +0900

    array map method test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({name, picture}){
+  //console.log(props.favortie)
+  return (
+    <div>
+      <h1>I like {name}</h1> 
+      <img src={picture}></img>
+    </div>
+  )
+}
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image: "https://newsimg.hankookilbo.com/2020/04/23/202004231500779914_1.jpg"
+  },
+  {
+    name: "Samgyeopsal",
+    image: "https://post-phinf.pstatic.net/MjAyMDAzMDNfMTcg/MDAxNTgzMTkwNjA3ODQ5.kUXPHqGJ2xPDSu_3FiEoFC3kY9QyQ_g9CziCGrFSDuEg.LpCfOTbc5qth9d-GKzGv9jwj2VKhcqmPHp5cp1KJYEsg.JPEG/IM_food02.jpg?type=w1200"
+  },
+  {
+    name: "Kimbap",
+    image: "https://i1.wp.com/seonkyounglongest.com/wp-content/uploads/2019/08/Korean-Eggroll-Kimbap5.jpg?fit=1300%2C867&ssl=1"
+  },
+  {
+    name: "Bibimbap",
+    image: "https://media.eggs.ca/assets/RecipeThumbs/_resampled/FillWyIxMjgwIiwiNzIwIl0/BIBINBAP-OVERHEAD.jpg"
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello!!!</h1>
+        {foodILike.map(item => (
+            <Food name={item.name} picture={item.image}/>
+          ))}
     </div>
   );
 }
